6110980c7f03c3f77b543b311918b997f22a193d
Magdiel Lopez <lopmag.lopez@gmail.com>
2022-08-28 22:14:49 -0600

    version base

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # imGenerator
 generar dummy images
+
+url http://localhost:3000/{ancho}x{alto}/{color}
+
+
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const { createCanvas } = require("canvas");
+const port = 3000;
+
+
+app.get('/:size/:bg', (req, res) => {
+    const { 0: width, 1: height } = req.params.size.split('x');
+    const canvas = createCanvas(Number(width), Number(height));
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#' + req.params.bg;
+    ctx.fillRect(0, 0, width, height);
+    const fontSize = (width < height) ? (width / 8) : (height / 8);
+    ctx.font = `${fontSize}px Arial`;
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.fillText(req.params.size, width / 2, (height / 2) + (fontSize / 2));
+    const buffer = canvas.toBuffer();
+    const headers = { "Content-Type": "image/png" };
+    res.writeHead(200, headers);
+    res.end(buffer);
+})
+
+app.listen(port, () => console.log(`Puerto: ${port}`));
