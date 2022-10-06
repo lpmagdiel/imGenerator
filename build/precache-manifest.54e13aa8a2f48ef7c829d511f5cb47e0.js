bc22a12f07a77fb0dd8f699ca14381e9a435a419
Magdiel Lopez <lopmag.lopez@gmail.com>
2022-10-05 19:16:55 -0600

    build 1

---
+++ build/precache-manifest.54e13aa8a2f48ef7c829d511f5cb47e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eaab0a7025a87811b7d",
+    "url": "/static/css/main.de5d1638.chunk.css"
+  },
+  {
+    "revision": "1eaab0a7025a87811b7d",
+    "url": "/static/js/main.1eaab0a7.chunk.js"
+  },
+  {
+    "revision": "8914e8e9fc2b344132e8",
+    "url": "/static/js/1.8914e8e9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d845e99f40a0312b2fa5456ec2715f4",
+    "url": "/static/media/IBMPlexMono-Bold.8d845e99.ttf"
+  },
+  {
+    "revision": "ad8e1092caf681575136f0af4d864927",
+    "url": "/static/media/IBMPlexMono-Regular.ad8e1092.ttf"
+  },
+  {
+    "revision": "46ad596b0e9327ad927bfe49c7759a98",
+    "url": "/index.html"
+  }
+];
